b3e1da1fd4e9c23960cd7e42186affa695ab444e
wangsl <123456>
2016-10-24 18:05:33 +0800

    use jade render data

--- routes/index.js
+++ routes/index.js
@@ -17,7 +17,7 @@ router.get('/', function(req, res, next) {
       });
     }
     log.daily = log.all.map(function (item,i) {
-      return i+1+'.'+item.message;
+      return item.message;
     });
     delete log.all;
     req.log = log;
@@ -25,7 +25,7 @@ router.get('/', function(req, res, next) {
   });
 });
 router.get('/', function(req, res) {
-  res.json(req.log);
+  res.render('index', { title: '日报',data:req.log });
 });
 router.get('/git', function(req, res, next) {
   simpleGit.log(["--since='1 day ago'","--author="+config.gitName],function(err,log){
